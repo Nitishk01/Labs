7fe77bbb053f8dba2f2f941e3f5e5038cbe7f727
Nitishk01 <knitish0103@gmail.com>
2022-03-14 13:27:51 +0530

    Initial commit

---
+++ src/Lab_1/Ex_5.java
@@ -0,0 +1,26 @@
+package Lab_1;
+
+import java.util.*;
+public class Ex_5 {
+
+	public static int calculateSum(int n){
+		int sum=0;
+		for(int i=1;i<=n;i++) {
+			if(i%5==0 || i%3==0) {
+				sum+=i;
+			}
+		}
+		return sum;
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the value of n");
+		int n = sc.nextInt();
+		System.out.println(calculateSum(n));
+		
+	sc.close();	
+	}
+}
+
